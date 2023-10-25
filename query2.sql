9fc24886f094b44241731f2cee28cb0ed5a350e4
LuisFFBM <101729535+LuisFFBM@users.noreply.github.com>
2023-10-25 10:11:54 +0200

    Add files via upload

--- query2.sql
+++ query2.sql
@@ -1,8 +1,13 @@
 
-
-SELECT bookings.book_ref, bookings.total_amount, SUM(amounts.amount) AS calc_amount
-FROM (SELECT tickets.book_ref, ticket_flights.amount 
-	FROM tickets JOIN ticket_flights ON tickets.ticket_no = ticket_flights.ticket_no) AS amounts
-JOIN bookings ON bookings.book_ref = amounts.book_ref
-GROUP BY bookings.book_ref
-ORDER BY bookings.book_ref ASC;
+SELECT bookings.book_ref,
+       bookings.total_amount,
+       Sum(amounts.amount) AS calc_amount
+FROM   (SELECT tickets.book_ref,
+               ticket_flights.amount
+        FROM   tickets
+               JOIN ticket_flights
+                 ON tickets.ticket_no = ticket_flights.ticket_no) AS amounts
+       JOIN bookings
+         ON bookings.book_ref = amounts.book_ref
+GROUP  BY bookings.book_ref
+ORDER  BY bookings.book_ref ASC; 
--- query3.sql
+++ query3.sql
@@ -1,20 +1,10 @@
-SELECT flights.arrival_airport, COUNT(tn.flight_id) AS pasajeros_recibidos
-FROM (SELECT t.ticket_no, t.flight_id
-	FROM ticket_flights AS t, boarding_passes AS b
-	where b.boarding_no != null and b.seat_no != null and t.flight_id = b.flight_id
-	) AS tn, flights 
-where tn.flight_id = flights.flight_id
-Group BY flights.arrival_airport
-ORDER BY pasajeros_recibidos ASC
-	
 
---SELECT f.arrival_airport, COUNT(f.ticket_no) as pasajeros
---FROM (flights join ticket_flights on flights.flight_id = ticket_flights.flight_id) as f
---GROUP BY f.arrival_airport
---ORDER BY pasajeros ASC;
 
---SELECT flights.arrival_airport, COUNT(flights.flight_id) AS pasajeros_recibidos
---FROM flights
---group by flights.arrival_airport
---ORDER by pasajeros_recibidos ASC
+SELECT f.arrival_airport AS "Código del Aeropuerto",
+       Count(bp.seat_no) AS "Número de Pasajeros Recibidos"
+FROM   flights AS f
+       JOIN boarding_passes AS bp
+         ON f.flight_id = bp.flight_id
+GROUP  BY f.arrival_airport
+ORDER  BY "número de pasajeros recibidos" ASC; 
 
